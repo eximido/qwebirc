cb6ae168dd2fef42b3d17a73952f1bc11f794f68
Kreon <eximido@users.noreply.github.com>
2023-08-12 14:15:31 +0300

    use newer versions of mootools
    now i have no idea why i've changed this years ago
    maybe it was a completely useless idea

--- bin/pages.py
+++ bin/pages.py
@@ -6,9 +6,9 @@ UI_BASE.extend(["panes/%s" % x for x in PANES])
 
 DEBUG_BASE = ["qwebirc", "version", "qhash", "jslib", "base64", "crypto", "md5", ["irc/%s" % x for x in IRC_BASE], ["ui/%s" % x for x in UI_BASE], "qwebircinterface", "auth", "sound"]
 BUILD_BASE = ["qwebirc"]
-JS_DEBUG_BASE = ["mootools-1.2.5-core-nc", "mootools-1.2.5.1-more-nc"]
+JS_DEBUG_BASE = ["MooTools-Core-1.6.0-compat", "MooTools-More-1.6.0-compat"]
 JS_RAW_BASE = []
-JS_BASE = ["mootools-1.2.5-core-nc", "mootools-1.2.5.1-more-nc"]
+JS_BASE = ["MooTools-Core-1.6.0-compat", "MooTools-More-1.6.0-compat"]
 JS_EXTRA = []
 
 UIs = {
